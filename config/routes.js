8d927fcb4b3ee5c36d3eee65f04d7bb4e73ee74a
Jordan Brauner <jordan.brauner@gmail.com>
2016-02-08 11:43:39 -0500

    test ignore

---
+++ config/routes.js
@@ -0,0 +1,16 @@
+module.exports = function() {
+
+  // HOME PAGE
+  app.get("/", function(req, res) {
+    res.send("index");
+  });
+
+  // LOGIN
+
+  // SIGNUP
+
+  // PROFILE
+
+  // LOGOUT
+
+};
