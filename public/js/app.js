c68422d076848b45639d44a544730770fc633797
Jordan Brauner <jordan.brauner@gmail.com>
2016-02-08 08:26:02 -0500

    minor changes. About to add authentication with passport

--- public/js/app.js
+++ public/js/app.js
@@ -14,6 +14,9 @@
   function RouterFunction($stateProvider, $rootScope) {
 
     $stateProvider
+      // Welcome route
+
+      // Robot routes
       .state("robotIndex", {
         url: "/robots",
         templateUrl: "js/robots/index.html",
@@ -37,6 +40,18 @@
         templateUrl: "js/robots/edit.html",
         controller: "RobotEditController",
         controllerAs: "RobotEditViewModel"
+      })
+
+      // Contribution routes
+
+      // User routes
+
+      // Roboto routes
+      .state("robotoAbout", {
+        url: "/about",
+        templateUrl: "js/roboto/about.html",
+        controller: "RobotoAboutController",
+        controllerAs: "RobotoAboutViewModel"
       });
   }
 
